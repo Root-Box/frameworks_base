edb032a6946d9c223eea678ab2f14fa0f8772cb1
Stevespear426 <stephen.k.spear@gmail.com>
2012-11-01 22:05:04 -0400

    WeatherPanel: Add toast and sim lock fix.
    Ok I can not test this since I don't own any devices with a sim card
    but, it looks like I placed the listener in a very odd place that
    only gets called if the sim state is unlocked, this i'm guessing here
    since I have no real data or report besides reboot, was causing a
    crash since the listener could be found when it came to initialize.
    
    note this is a review site, not a bug tracker, I HIGHLY recommend
    you sim card peeps try this and give feed back because this will get merged
    quickly since its just a toast message that work as intended and this fix
    is just a bonus..
    
    added collapse after click weather refresh.
    
    added weather, calendar, and custom lockscreen background stuffs...
    I cant check the layout since I cnt lock my sim, so you guys might have to
    play with these to make verything look right.
    
    Change-Id: I487c63a341fb1f121187ef3224b88afabdffba3d

--- policy/src/com/android/internal/policy/impl/KeyguardStatusViewManager.java
+++ policy/src/com/android/internal/policy/impl/KeyguardStatusViewManager.java
@@ -55,6 +55,7 @@ import android.provider.Settings;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 /***
@@ -482,6 +483,16 @@ class KeyguardStatusViewManager implements OnClickListener {
             }
         }
     }
+
+    private View.OnClickListener mWeatherListener = new View.OnClickListener() {
+        public void onClick(View v) {
+             Intent weatherintent = new Intent("com.aokp.romcontrol.INTENT_WEATHER_REQUEST");
+             weatherintent.putExtra("com.aokp.romcontrol.INTENT_EXTRA_TYPE", "updateweather");
+             weatherintent.putExtra("com.aokp.romcontrol.INTENT_EXTRA_ISMANUAL", true);
+             v.getContext().sendBroadcast(weatherintent);
+             Toast.makeText(getContext(), R.string.update_weather, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     private void updateCalendar() {
         ContentResolver resolver = getContext().getContentResolver();
@@ -958,15 +969,6 @@ class KeyguardStatusViewManager implements OnClickListener {
         }
     }
 
-    private View.OnClickListener mWeatherListener = new View.OnClickListener() {
-        public void onClick(View v) {
-             Intent weatherintent = new Intent("com.aokp.romcontrol.INTENT_WEATHER_REQUEST");
-             weatherintent.putExtra("com.aokp.romcontrol.INTENT_EXTRA_TYPE", "updateweather");
-             weatherintent.putExtra("com.aokp.romcontrol.INTENT_EXTRA_ISMANUAL", true);
-             v.getContext().sendBroadcast(weatherintent);
-
-        }
-    };
 
     /**
      * Performs concentenation of PLMN/SPN
