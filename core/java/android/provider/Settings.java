9f38665be968947543ecb16c5b830829122d7e75
Jubakuba <jubakuba@gmail.com>
2013-01-03 00:52:29 -0700

    Framework: NavBarColor (1/2)
    Change-Id: I7ed579d95d921e08d8f9d0a9160605457b8c4cb3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3405,6 +3405,13 @@ public final class Settings {
          */
         public static final String NAVIGATION_BAR_ALPHA = "navigation_bar_alpha";
 
+        /**
+         * Navigation bar color.
+         *
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_COLOR = "navigation_bar_color";
+
     }
 
     /**
