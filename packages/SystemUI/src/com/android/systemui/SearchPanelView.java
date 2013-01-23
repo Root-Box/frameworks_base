6fda5bb78edfc2334b955151d5058190b01c70c4
Roman Birg <romanbirg@gmail.com>
2013-01-23 15:28:20 -0600

    SystemUI: fix long-press navring targets not opening in keyguard
    Change-Id: I80d63b7a9b8429d19bcad5a15419c9e72def1f70
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -213,6 +213,7 @@ public class SearchPanelView extends FrameLayout implements
                     mLongPress = true;
                     Log.d(TAG,"LongPress!");
                     mBar.hideSearchPanel();
+                    maybeSkipKeyguard();
                     AwesomeAction.getInstance(mContext).launchAction(longList.get(mTarget));
                     mSearchPanelLock = true;
                  }
@@ -254,12 +255,7 @@ public class SearchPanelView extends FrameLayout implements
                 if (AwesomeAction.ACTION_ASSIST.equals(intentList.get(target))) {
                     startAssistActivity();
                 } else {
-                    try {
-                        if (mWm.isKeyguardLocked() && !mWm.isKeyguardSecure()) {
-                            ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
-                        }
-                    } catch (RemoteException ignored) {
-                    }
+                    maybeSkipKeyguard();
                     AwesomeAction.getInstance(mContext).launchAction(intentList.get(target));
                 }
                 mHandler.removeCallbacks(SetLongPress);
@@ -293,6 +289,15 @@ public class SearchPanelView extends FrameLayout implements
 
         updateSettings();
         setDrawables();
+    }
+
+    private void maybeSkipKeyguard() {
+        try {
+            if (mWm.isKeyguardLocked() && !mWm.isKeyguardSecure()) {
+                ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+            }
+        } catch (RemoteException ignored) {
+        }
     }
 
     private void setDrawables() {
