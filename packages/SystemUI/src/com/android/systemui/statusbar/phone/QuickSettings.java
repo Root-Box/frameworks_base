4af8a85a3db8af98319db277dba270415d7c9a08
Bajee11 <bajee11@gmail.com>
2013-02-06 23:58:45 -0500

    QS: Fix Brightness, Favorite Contact & User tile FC in TabletUI

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -509,7 +509,7 @@ public class QuickSettings {
                 quick.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        mBar.collapseAllPanels(true);
+                        getService().animateCollapsePanels();
                         final UserManager um =
                                 (UserManager) mContext.getSystemService(Context.USER_SERVICE);
                         if (um.getUsers(true).size() > 1) {
@@ -634,7 +634,7 @@ public class QuickSettings {
                 quick.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        mBar.collapseAllPanels(true);
+                        getService().animateCollapsePanels();
                         showBrightnessDialog();
                     }
                 });
@@ -1369,7 +1369,7 @@ public class QuickSettings {
                         Settings.System.QUICK_TOGGLE_FAV_CONTACT);
 
                         if (lookupKey != null && lookupKey.length() > 0) {
-                            mBar.collapseAllPanels(true);
+                            getService().animateCollapsePanels();
                             Uri lookupUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI, lookupKey);
                             Uri res = ContactsContract.Contacts.lookupContact(mContext.getContentResolver(), lookupUri);
                             Intent intent = ContactsContract.QuickContact.composeQuickContactsIntent(
