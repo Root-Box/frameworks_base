15f0604f2a274458035bd1cd13aefe81182deed5
aaronpoweruser <atlrudeboi1@gmail.com>
2013-03-07 10:14:49 -0800

    PWM: check for expanded desktop on boot
    Change-Id: I8f3883019b60138d8937bd85adc9231b0e18c595

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1202,6 +1202,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         } catch (RemoteException ex) { }
 
         mDisableOverlays = updateFlingerOptions();
+        updateHWOverlays();
         updateHybridLayout();
 
         mSettingsObserver = new SettingsObserver(mHandler);
