a2997925be6c7b0b000bc584ce92b12e47cc32bc
Daniel <daxx4182@gmail.com>
2013-03-13 19:41:26 +0800

    Toggle: Volume Toggle
    Based on CM's - https://github.com/CyanogenMod/android_frameworks_base/commit/d2b7a0e0b0c5a6fb350214858a9f2468fd3b9e60
    
    Signed-off-by: Daniel <daxx4182@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -81,6 +81,7 @@ public class ToggleManager {
     public static final String LTE_TOGGLE = "LTE";
     public static final String FAV_CONTACT_TOGGLE = "FAVCONTACT";
     public static final String SOUND_STATE_TOGGLE = "SOUNDSTATE";
+    public static final String VOLUME_TOGGLE = "VOLUME";
     public static final String NAVBAR_HIDE_TOGGLE = "NAVBARHIDE";
     public static final String QUICKRECORD_TOGGLE = "QUICKRECORD";
     public static final String POWERMENU_TOGGLE = "POWERMENU";
@@ -134,6 +135,7 @@ public class ToggleManager {
             toggleMap.put(LTE_TOGGLE, LteToggle.class);
             toggleMap.put(FAV_CONTACT_TOGGLE, FavoriteUserToggle.class);
             toggleMap.put(SOUND_STATE_TOGGLE, SoundStateToggle.class);
+            toggleMap.put(VOLUME_TOGGLE, VolumeToggle.class);
             toggleMap.put(NAVBAR_HIDE_TOGGLE, NavbarHideToggle.class);
             toggleMap.put(QUICKRECORD_TOGGLE, QuickRecordToggle.class);
             toggleMap.put(POWERMENU_TOGGLE, PowerMenuToggle.class);
