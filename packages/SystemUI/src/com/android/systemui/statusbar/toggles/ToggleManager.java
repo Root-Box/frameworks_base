57deda62c1e1220f3b374e08dd6d9d911539524e
Bajee11 <bajee11@gmail.com>
2013-03-17 02:05:55 -0400

    Dark RootBox Toggle
    Icon credits: @kroz

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -91,6 +91,7 @@ public class ToggleManager {
     public static final String ROOTBOX_TOGGLE = "ROOTBOX";
     public static final String SLEEP_TOGGLE = "SLEEP";
     public static final String PIE_TOGGLE = "PIE";
+    public static final String DARK_TOGGLE = "DARK";
 
     private int mStyle;
 
@@ -145,6 +146,7 @@ public class ToggleManager {
             toggleMap.put(ROOTBOX_TOGGLE, RootBoxToggle.class);
             toggleMap.put(SLEEP_TOGGLE, SleepToggle.class);
             toggleMap.put(PIE_TOGGLE, PieToggle.class);
+            toggleMap.put(DARK_TOGGLE, DarkToggle.class);
             // toggleMap.put(BT_TETHER_TOGGLE, null);
         }
         return toggleMap;
