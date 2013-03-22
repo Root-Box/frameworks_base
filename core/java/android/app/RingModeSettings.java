CyanogenMod/android_frameworks_base

Framework: Call Ring mode what Google calls it, not Silent mode (2 of 2)
Silent mode -> Ring mode cleanup

Change-Id: I929f7d1699b623b4a93608dd32786ebc2691b51b

core/java/android/app/RingModeSettings.java
@@ -11,8 +11,8 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 
 /** @hide */
-public final class SilentModeSettings implements Parcelable {
-    private static final String SILENT_MODE_OFF = "off";
+public final class RingModeSettings implements Parcelable {
+    private static final String SILENT_MODE_NORMAL = "normal";
     private static final String SILENT_MODE_VIBRATE = "vibrate";
     private static final String SILENT_MODE_MUTE = "mute";
 
@@ -21,27 +21,27 @@ public final class SilentModeSettings implements Parcelable {
     private boolean mDirty;
 
     /** @hide */
-    public static final Parcelable.Creator<SilentModeSettings> CREATOR = new Parcelable.Creator<SilentModeSettings>() {
-        public SilentModeSettings createFromParcel(Parcel in) {
-            return new SilentModeSettings(in);
+    public static final Parcelable.Creator<RingModeSettings> CREATOR = new Parcelable.Creator<RingModeSettings>() {
+        public RingModeSettings createFromParcel(Parcel in) {
+            return new RingModeSettings(in);
         }
 
         @Override
-        public SilentModeSettings[] newArray(int size) {
-            return new SilentModeSettings[size];
+        public RingModeSettings[] newArray(int size) {
+            return new RingModeSettings[size];
         }
     };
 
 
-    public SilentModeSettings(Parcel parcel) {
+    public RingModeSettings(Parcel parcel) {
         readFromParcel(parcel);
     }
 
-    public SilentModeSettings() {
-        this(SILENT_MODE_OFF, false);
+    public RingModeSettings() {
+        this(SILENT_MODE_NORMAL, false);
     }
 
-    public SilentModeSettings(String value, boolean override) {
+    public RingModeSettings(String value, boolean override) {
         mValue = value;
         mOverride = override;
         mDirty = false;
@@ -84,10 +84,10 @@ public final class SilentModeSettings implements Parcelable {
     }
 
     /** @hide */
-    public static SilentModeSettings fromXml(XmlPullParser xpp, Context context)
+    public static RingModeSettings fromXml(XmlPullParser xpp, Context context)
             throws XmlPullParserException, IOException {
         int event = xpp.next();
-        SilentModeSettings connectionDescriptor = new SilentModeSettings();
+        RingModeSettings connectionDescriptor = new RingModeSettings();
         while (event != XmlPullParser.END_TAG) {
             if (event == XmlPullParser.START_TAG) {
                 String name = xpp.getName();

Root-Box/frameworks_base

core/java/android/app/RingModeSettings.java
@@ -11,37 +11,37 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 
 /** @hide */
-public final class SilentModeSettings implements Parcelable {
-    private static final String SILENT_MODE_OFF = "off";
-    private static final String SILENT_MODE_VIBRATE = "vibrate";
-    private static final String SILENT_MODE_MUTE = "mute";
+public final class RingModeSettings implements Parcelable {
+    private static final String RING_MODE_NORMAL = "normal";
+    private static final String RING_MODE_VIBRATE = "vibrate";
+    private static final String RING_MODE_MUTE = "mute";
 
     private String mValue;
     private boolean mOverride;
     private boolean mDirty;
 
     /** @hide */
-    public static final Parcelable.Creator<SilentModeSettings> CREATOR = new Parcelable.Creator<SilentModeSettings>() {
-        public SilentModeSettings createFromParcel(Parcel in) {
-            return new SilentModeSettings(in);
+    public static final Parcelable.Creator<RingModeSettings> CREATOR = new Parcelable.Creator<RingModeSettings>() {
+        public RingModeSettings createFromParcel(Parcel in) {
+            return new RingModeSettings(in);
         }
 
         @Override
-        public SilentModeSettings[] newArray(int size) {
-            return new SilentModeSettings[size];
+        public RingModeSettings[] newArray(int size) {
+            return new RingModeSettings[size];
         }
     };
 
 
-    public SilentModeSettings(Parcel parcel) {
+    public RingModeSettings(Parcel parcel) {
         readFromParcel(parcel);
     }
 
-    public SilentModeSettings() {
-        this(SILENT_MODE_OFF, false);
+    public RingModeSettings() {
+        this(RING_MODE_NORMAL, false);
     }
 
-    public SilentModeSettings(String value, boolean override) {
+    public RingModeSettings(String value, boolean override) {
         mValue = value;
         mOverride = override;
         mDirty = false;
@@ -73,9 +73,9 @@ public final class SilentModeSettings implements Parcelable {
     public void processOverride(Context context) {
         if (isOverride()) {
             int ringerMode = AudioManager.RINGER_MODE_NORMAL;
-            if (mValue.equals(SILENT_MODE_MUTE)) {
+            if (mValue.equals(RING_MODE_MUTE)) {
                 ringerMode = AudioManager.RINGER_MODE_SILENT;
-            } else if (mValue.equals(SILENT_MODE_VIBRATE)) {
+            } else if (mValue.equals(RING_MODE_VIBRATE)) {
                 ringerMode = AudioManager.RINGER_MODE_VIBRATE;
             }
             AudioManager amgr = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
@@ -84,10 +84,10 @@ public final class SilentModeSettings implements Parcelable {
     }
 
     /** @hide */
-    public static SilentModeSettings fromXml(XmlPullParser xpp, Context context)
+    public static RingModeSettings fromXml(XmlPullParser xpp, Context context)
             throws XmlPullParserException, IOException {
         int event = xpp.next();
-        SilentModeSettings connectionDescriptor = new SilentModeSettings();
+        RingModeSettings connectionDescriptor = new RingModeSettings();
         while (event != XmlPullParser.END_TAG) {
             if (event == XmlPullParser.START_TAG) {
                 String name = xpp.getName();
@@ -104,11 +104,11 @@ public final class SilentModeSettings implements Parcelable {
 
     /** @hide */
     public void getXmlString(StringBuilder builder, Context context) {
-        builder.append("<silentModeDescriptor>\n<value>");
+        builder.append("<ringModeDescriptor>\n<value>");
         builder.append(mValue);
         builder.append("</value>\n<override>");
         builder.append(mOverride);
-        builder.append("</override>\n</silentModeDescriptor>\n");
+        builder.append("</override>\n</ringModeDescriptor>\n");
     }
 
     @Override