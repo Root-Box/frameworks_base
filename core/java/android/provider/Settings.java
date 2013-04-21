8a2014072f0bfbf57d1cc7e02f82130754f9b1a0
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-04-21 22:59:43 +0700

    Option to use volume keys to control media volume anytime (2/2)
    See Settings part for description
    
    Change-Id: I78d26b0bd41f5b58514ae9c68208a8c950396823

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4383,6 +4383,13 @@ public final class Settings {
         public static final String MISSED_CALL_BREATH = "missed_call_breath";
 
         /**
+         * Volume key controls ringtone or media sound stream
+         *
+         * @hide
+         */
+        public static final String VOLUME_KEYS_CONTROL_RING_STREAM = "volume_keys_control_ring_stream";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -451,6 +451,8 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
 
     private int mDockState = Intent.EXTRA_DOCK_STATE_UNDOCKED;
 
+    private boolean mVolumeKeysControlRingStream;
+
     ///////////////////////////////////////////////////////////////////////////
     // Construction
     ///////////////////////////////////////////////////////////////////////////
@@ -765,6 +767,9 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
 
             readDockAudioSettings(cr);
             updateManualSafeMediaVolume();
+
+            mVolumeKeysControlRingStream = Settings.System.getInt(cr,
+                    Settings.System.VOLUME_KEYS_CONTROL_RING_STREAM, 1) == 1;
         }
         boolean linkNotificationWithVolume = Settings.System.getInt(mContentResolver,
                 Settings.System.VOLUME_LINK_NOTIFICATION, 1) == 1;
@@ -2551,9 +2556,15 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                         Log.v(TAG, "getActiveStreamType: Forcing STREAM_MUSIC stream active");
                     return AudioSystem.STREAM_MUSIC;
                 } else {
-                    if (DEBUG_VOL)
-                        Log.v(TAG, "getActiveStreamType: Forcing STREAM_RING b/c default");
-                    return AudioSystem.STREAM_RING;
+                    if (!mVolumeKeysControlRingStream) {
+                        if (DEBUG_VOL)
+                            Log.v(TAG, "getActiveStreamType: Forcing STREAM_RING b/c default");
+                        return AudioSystem.STREAM_RING;
+                    } else {
+                        if (DEBUG_VOL)
+                            Log.v(TAG, "getActiveStreamType: Forcing STREAM_MUSIC b/c default setting");
+                        return AudioSystem.STREAM_MUSIC;
+                    }
                 }
             } else if (AudioSystem.isStreamActive(AudioSystem.STREAM_MUSIC, 0)) {
                 if (DEBUG_VOL)
@@ -3557,6 +3568,8 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                     Settings.System.VOLUME_LINK_NOTIFICATION), false, this);
             mContentResolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.MANUAL_SAFE_MEDIA_VOLUME), false, this);
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                Settings.System.VOLUME_KEYS_CONTROL_RING_STREAM), false, this);
         }
 
         @Override
@@ -3609,6 +3622,8 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                 } else {
                     mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_NOTIFICATION;
                 }
+                    mVolumeKeysControlRingStream = Settings.System.getIntForUser(mContentResolver,
+                            Settings.System.VOLUME_KEYS_CONTROL_RING_STREAM, 1) == 1;
             }
         }
     }
