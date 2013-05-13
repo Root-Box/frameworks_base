a8af90d329ba87608b6797e15522150d6bc45819
Reid Holland <reid.holland80@gmail.com>
2013-05-12 22:43:11 -0500

    Add BAMF theme engine
    Thanks to travp624 @ TeamBaked for originally porting this
    
    Change-Id: I9dd9347ac62e3b1fb49a4562d84eb7b295cdc2b2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1027,6 +1027,20 @@ public final class Settings {
         }
 
         /**
+         * Look up a name in the database.
+         * @param resolver to access the database with
+         * @param name to look up in the table
+         * @param defaultValue returned if value is null
+         * @return the corresponding value, or default if not present
+         */
+        public synchronized static String getString(ContentResolver resolver,
+                String name, String defaultValue) {
+
+            String value = getString(resolver, name);
+            return value == null ? defaultValue: value;
+        }
+
+        /**
          * Store a name/value pair into the database.
          * @param resolver to access the database with
          * @param name to store
@@ -4062,6 +4076,8 @@ public final class Settings {
          * @hide
          */
         public static final String MVNO_ROAMING = "mvno_roaming";
+
+        public static final String THEME_WALLPAPER = "theme_wallpaper";
 
         /**
          * Forces formal text input.  1 to replace emoticon key with enter key.
