10cf47e9ba2042920d1e6dc922649ebc0d3dfe94
Lars Greiss <kufikugel@googlemail.com>
2013-05-15 01:26:33 +0200

    Framworks: missed some stuff
    tested it with right change but submitted wrong one
    sorry
    
    Change-Id: Ia50672d038348b11a8083317b40b162770436ed0

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java
@@ -162,7 +162,6 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
     private IWindowManager mWm;
     private int mBatteryLevel;
     private int mBatteryStatus;
-    private boolean mHasTelephony;
     private TelephonyManager mTelephonyManager;
     private ServiceState mServiceState;
     private ActivityManager mActivityManager;
@@ -1082,7 +1081,7 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
     }
 
     public String getOperatorState() {
-        if (!mHasTelephony) {
+        if (mTelephonyManager == null) {
             return null;
         }
         if (mServiceState == null || mServiceState.getState() == ServiceState.STATE_OUT_OF_SERVICE) {
