686c5134dcd30a74103e09ff03e3129f3d09f624
Jonathan Steadman <xxwhitehawk@gmail.com>
2013-05-24 12:06:20 -0700

    SystemUI: show blue dc-hspap icon when connected to Google services
    *also fixed some tabs
    
    Change-Id: I6f9910978a38622eb76758d2b806a1cc49d02496
    Signed-off-by: Jonathan Steadman <xxwhitehawk@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -684,7 +684,7 @@ public class NetworkController extends BroadcastReceiver {
                     break;
                 case TelephonyManager.NETWORK_TYPE_DCHSPAP:
                     mDataIconList = TelephonyIcons.DATA_DC[mInetCondition];
-                    mDataTypeIconId = R.drawable.stat_sys_data_connected_dc;
+                    mDataTypeIconId = mDataIconList[0];
                     mContentDescriptionDataType = mContext.getString(
                             R.string.accessibility_data_connection_DC);
                     break;
@@ -693,12 +693,12 @@ public class NetworkController extends BroadcastReceiver {
                         // display 1xRTT for IS95A/B
                         mDataIconList = mUseAltSignal ? TelephonyIcons.DATA_1X_ALT[mInetCondition] :
                         TelephonyIcons.DATA_1X[mInetCondition];
-                    	mDataTypeIconId = mDataIconList[0];
+                        mDataTypeIconId = mDataIconList[0];
                         mQSDataTypeIconId = (isConnected ? R.drawable.ic_qs_signal_full_1x
                                 : R.drawable.ic_qs_signal_1x);
                         mContentDescriptionDataType = mContext.getString(
                             R.string.accessibility_data_connection_cdma);
-						break;
+                    break;
                     } else {
                         // fall through
                     }
@@ -706,7 +706,7 @@ public class NetworkController extends BroadcastReceiver {
                     if (!mShowAtLeastThreeGees) {
                         mDataIconList = mUseAltSignal ? TelephonyIcons.DATA_1X_ALT[mInetCondition] :
                         TelephonyIcons.DATA_1X[mInetCondition];
-                    	mDataTypeIconId = mDataIconList[0];
+                        mDataTypeIconId = mDataIconList[0];
                         mQSDataTypeIconId = (isConnected ? R.drawable.ic_qs_signal_full_1x
                                 : R.drawable.ic_qs_signal_1x);
                         mContentDescriptionDataType = mContext.getString(
