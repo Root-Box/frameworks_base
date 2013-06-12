a401b7d11e82a620fadb40a2ed07857d8558d41e
Steve Kondik <shade@chemlab.org>
2013-06-12 00:47:55 -0700

    Privacy Guard

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -48,6 +48,7 @@ import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Binder;
 import android.os.Process;
 import android.os.RemoteException;
 import android.util.Log;
@@ -1294,6 +1295,7 @@ final class ApplicationPackageManager extends PackageManager {
     }
 
     @Override
+<<<<<<< HEAD
     public String[] getRevokedPermissions(String packageName) {
         try {
             return mPM.getRevokedPermissions(packageName);
@@ -1310,6 +1312,24 @@ final class ApplicationPackageManager extends PackageManager {
         } catch (RemoteException e) {
             // Should never happen!
         }
+=======
+    public void setPrivacyGuardSetting(String packageName, boolean enabled) {
+        try {
+            mPM.setPrivacyGuardSetting(packageName, enabled, mContext.getUserId());
+        } catch (RemoteException e) {
+            // Should never happen!
+        }
+    }
+
+    @Override
+    public boolean getPrivacyGuardSetting(String packageName) {
+        try {
+            return mPM.getPrivacyGuardSetting(packageName, mContext.getUserId());
+        } catch (RemoteException e) {
+            // Should never happen!
+        }
+        return false;
+>>>>>>> e48fce6... framework: Privacy Guard
     }
 
     /**
