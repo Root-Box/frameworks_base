3da0e002bb8c0d24e4ae5e807ce0ce47a1851c0b
Bajee11 <bajee11@gmail.com>
2013-06-22 19:34:16 -0400

    FW: Open RB settings as a floating window
    This will open up RootBox settings in a floating window when RootBox toggle is pressed, long pressing will open through full settings window.

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/RootBoxToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/RootBoxToggle.java
@@ -19,13 +19,25 @@ public class RootBoxToggle extends BaseToggle {
 
     @Override
     public void onClick(View v) {
+        try {
+            Intent intent = new Intent("android.intent.action.MAIN");
+            intent.setClassName("com.android.settings", "com.android.settings.Settings$RootboxActivity");
+            intent.addFlags(Intent.FLAG_FLOATING_WINDOW);
+            startActivity(intent);
+        } catch(NullPointerException e) {
+            // No intent found for activity component
+        }
+        collapseStatusBar();
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
         Intent intent = new Intent("android.intent.action.MAIN");
         intent.setClassName("com.android.settings", "com.android.settings.Settings$RootboxActivity");
         intent.addCategory("android.intent.category.LAUNCHER");
-
         collapseStatusBar();
-        dismissKeyguard();
         startActivity(intent);
+        collapseStatusBar();
+        return super.onLongClick(v);
     }
-
 }
