e4bd2cd1d2e23ccba0337ec138702efc85152564
Daniel MM <daxx4182@gmail.com>
2013-07-13 19:47:40 +0800

    HALO: Speech bubble, fix padding

--- core/java/com/android/internal/widget/LockPatternUtils.java
+++ core/java/com/android/internal/widget/LockPatternUtils.java
@@ -1404,8 +1404,8 @@ public class LockPatternUtils {
                 || mode == DevicePolicyManager.PASSWORD_QUALITY_COMPLEX;
         final boolean isProfileSecure = mProfileManager.getActiveProfile().getScreenLockModeWithDPM(mContext) == Profile.LockMode.DEFAULT;
         final boolean secure = (isPattern && isLockPatternEnabled() && savedPatternExists()
+                || isGesture && isLockGestureEnabled() && savedGestureExists()
                 || isPassword && savedPasswordExists()) && isProfileSecure;
-                || isGesture && isLockGestureEnabled() && savedGestureExists()) && isProfileSecure;
         return secure;
     }
 
